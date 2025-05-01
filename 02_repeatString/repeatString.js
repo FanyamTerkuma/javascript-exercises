8b1b13090c0c61340337f5948af67fd02e02fa74
Fanyam Terkuma <lawrencefanyam@gmail.com>
2025-05-01 19:08:09 +0100

    Refactor repeatString function to include parameters and handle negative repeats

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (receivedString, numberOfRepeats) {
+    let repeatedString = '';
 
+    if (numberOfRepeats < 0) {
+        return 'ERROR';
+    }
+    for (i = 0; i < numberOfRepeats; i++) {
+        repeatedString = repeatedString + receivedString;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
