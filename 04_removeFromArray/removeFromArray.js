b2ec5fdc02b78a437f9709ff44eff85fc2465a16
Fanyam Terkuma <lawrencefanyam@gmail.com>
2025-05-02 13:58:10 +0100

    Simplify removeFromArray function by removing unnecessary conditional logic for single item removal

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,19 +1,10 @@
 const removeFromArray = function (inputArray, ...itemsToRemove) {
     let filteredArray = [];
-    if (itemsToRemove.length > 1) {
-        for (const item of inputArray) {
-            if (!itemsToRemove.includes(item)) {
-                filteredArray.push(item);
-            }
-        }
-    } else {
-        for (const item of inputArray) {
-            if (item !== itemsToRemove[0]) {
-                filteredArray.push(item);
-            }
+    for (const item of inputArray) {
+        if (!itemsToRemove.includes(item)) {
+            filteredArray.push(item);
         }
     }
-
     return filteredArray;
 };
 
