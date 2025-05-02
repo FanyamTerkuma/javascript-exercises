5f8d3a562868d496910b437cf8d8962dd215bdcf
Fanyam Terkuma <lawrencefanyam@gmail.com>
2025-05-02 14:52:11 +0100

    Implement sumAll function to calculate the sum of all integers between two specified numbers with input validation

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function (firstNumber, lastNumber) {
+    if (firstNumber < 0 || lastNumber < 0) return 'ERROR';
+    if (!Number.isInteger(firstNumber) || !Number.isInteger(lastNumber)) return 'ERROR';
 
+    // array destructing to swap the values of the firstNumber and lastNumber if the firstNumber is larger
+    if (firstNumber > lastNumber) [firstNumber, lastNumber] = [lastNumber, firstNumber];
+
+    let sum = 0;
+    for (let i = firstNumber; i <= lastNumber; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
