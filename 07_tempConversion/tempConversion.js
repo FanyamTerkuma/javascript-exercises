354afeadf7b41933310410dc5b9858ead8d73929
Fanyam Terkuma <lawrencefanyam@gmail.com>
2025-05-23 15:58:39 +0100

    Completed Temperature Conversions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+    let tempInCelsius = temp === 32 ? 0 : (temp - 32) * 5 / 9;
+    return Number(tempInCelsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+    let tempInFahrenheit = temp === 0 ? 32 : temp * 9 / 5 + 32;
+    return Number(tempInFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+    convertToCelsius,
+    convertToFahrenheit
 };
